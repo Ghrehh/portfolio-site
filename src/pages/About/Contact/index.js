d60711cd34367bbf3a9d99bfb5294bfb4faef336
Gerard Burns <gjburns9@hotmail.com>
2018-04-04 20:12:54 +0100

    about page contact clean up

--- src/pages/About/Contact/index.js
+++ src/pages/About/Contact/index.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 import Heading from 'components/Heading';
 import Button from 'components/Button';
-import styles from './styles.module.css';
 
-const buttonStyle = additionalStyle => `${styles.button} ${additionalStyle}`;
+import styles from './styles.module.css';
 
 const Contact = () => (
   <React.Fragment>
     <div className={styles.slantedBackground} />
+
     <section className={styles.contact}>
       <div className={styles.contactInner}>
         <div className={styles.textContainer}>
           <Heading className={styles.heading}>CONTACT</Heading>
-          <p className={styles.body}>Feel free to get in touch.</p>
+          <h2 className={styles.subHeading}>Feel free to get in touch.</h2>
         </div>
 
         <div>
-          <Button className={buttonStyle(styles.twitterButton)}>Twitter</Button>
-          <Button className={buttonStyle(styles.emailButton)}>Email</Button>
+          <Button className={styles.twitterButton}>Twitter</Button>
+          <Button className={styles.emailButton}>Email</Button>
         </div>
       </div>
     </section>
