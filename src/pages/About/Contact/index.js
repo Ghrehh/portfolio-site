187c9e734b7368a354393467f11c75bf4f4402ea
Gerard Burns <gjburns9@hotmail.com>
2018-04-04 19:59:56 +0100

    move slanted background to contact

--- src/pages/About/Contact/index.js
+++ src/pages/About/Contact/index.js
@@ -7,19 +7,22 @@ import styles from './styles.module.css';
 const buttonStyle = additionalStyle => `${styles.button} ${additionalStyle}`;
 
 const Contact = () => (
-  <section className={styles.contact}>
-    <div className={styles.contactInner}>
-      <div className={styles.textContainer}>
-        <Heading className={styles.heading}>CONTACT</Heading>
-        <p className={styles.body}>Feel free to get in touch.</p>
-      </div>
+  <React.Fragment>
+    <div className={styles.slantedBackground} />
+    <section className={styles.contact}>
+      <div className={styles.contactInner}>
+        <div className={styles.textContainer}>
+          <Heading className={styles.heading}>CONTACT</Heading>
+          <p className={styles.body}>Feel free to get in touch.</p>
+        </div>
 
-      <div>
-        <Button className={buttonStyle(styles.twitterButton)}>Twitter</Button>
-        <Button className={buttonStyle(styles.emailButton)}>Email</Button>
+        <div>
+          <Button className={buttonStyle(styles.twitterButton)}>Twitter</Button>
+          <Button className={buttonStyle(styles.emailButton)}>Email</Button>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </React.Fragment>
 );
 
 export default Contact;
--- src/pages/About/Resume/index.js
+++ src/pages/About/Resume/index.js
@@ -8,28 +8,24 @@ import styles from './styles.module.css';
 const buttonStyle = additionalStyle => `${styles.button} ${additionalStyle}`;
 
 const Resume = () => (
-  <React.Fragment>
-    <section className={styles.resume}>
-      <div className={styles.resumeInner}>
-        <Heading className={styles.heading}>CV/RESUME</Heading>
+  <section className={styles.resume}>
+    <div className={styles.resumeInner}>
+      <Heading className={styles.heading}>CV/RESUME</Heading>
 
-        <p className={styles.body}>
-          Please view my CV for a detailed description of my past and current
-          work.
-        </p>
+      <p className={styles.body}>
+        Please view my CV for a detailed description of my past and current
+        work.
+      </p>
 
-        <ButtonContainer className={styles.buttonContainer}>
-          <Button className={buttonStyle(styles.viewButton)}>View CV</Button>
+      <ButtonContainer className={styles.buttonContainer}>
+        <Button className={buttonStyle(styles.viewButton)}>View CV</Button>
 
-          <Button className={buttonStyle(styles.downloadButton)}>
-            Download CV
-          </Button>
-        </ButtonContainer>
-      </div>
-    </section>
-
-    <div className={styles.slantedBackground} />
-  </React.Fragment>
+        <Button className={buttonStyle(styles.downloadButton)}>
+          Download CV
+        </Button>
+      </ButtonContainer>
+    </div>
+  </section>
 );
 
 export default Resume;
