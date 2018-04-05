533e2fc682e97cbc73d4d0f4a492e5249437ae8c
Gerard Burns <gjburns9@hotmail.com>
2018-04-06 00:19:41 +0100

    fix button style

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,7 +5,7 @@ import * as childrenAndClassName from 'models/children-and-class-name';
 import styles from './styles.module.css';
 
 const Button = (props: childrenAndClassName.Type) => (
-  <button className={`${styles.heading} ${props.className}`}>
+  <button className={`${styles.button} ${props.className}`}>
     {props.children}
   </button>
 );
