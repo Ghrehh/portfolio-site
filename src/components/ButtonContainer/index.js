72124ef00db9924773ab62afdeaca5f5e0075bcc
Gerard Burns <gjburns9@hotmail.com>
2018-04-07 15:46:48 +0100

    button container cleanup

--- src/components/ButtonContainer/index.js
+++ src/components/ButtonContainer/index.js
@@ -1,18 +1,17 @@
 // @flow
-import React from 'react';
-import type { Element } from 'react';
+import * as React from 'react';
 
 import Button from 'components/Button';
 import styles from './styles.module.css';
 
 type Props = {
-  children: Element<typeof Button>[],
+  children: React.Element<typeof Button>[],
   className?: string
 };
 
 class ButtonContainer extends React.Component<Props> {
   renderChildren = () =>
-    React.Children.toArray(this.props.children).map((button, index) => (
+    this.props.children.map((button, index) => (
       <div className={styles.buttonSpacer} key={index}>
         {button}
       </div>
