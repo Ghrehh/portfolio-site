2df2e5f1a1f75c525c296c60b8642013f6fd555d
Gerard Burns <gjburns9@hotmail.com>
2018-04-15 01:13:51 +0100

    tweaked copy

--- src/data.js
+++ src/data.js
@@ -4,21 +4,22 @@ export default {
   projects: {
     'tweetatlas-client': {
       name: 'TWEETATLAS CLIENT',
-      description: 'Client used to display data from the Tweetatlas Server',
+      description:
+        'Client used to display data from the Tweetatlas Server, built using Vue JS.',
       backgroundColor: 'var(--green)',
       gitHubLink: 'https://github.com/Ghrehh/tweetatlas-client'
     },
     'tweetatlas-server': {
       name: 'TWEETATLAS SERVER',
       description:
-        'Service used to estimate the location of users creating tweets container a search phrase',
+        'Service used to estimate the location of users creating tweets containing a search phrase or phrases. Built using Go.',
       backgroundColor: 'var(--yellow)',
       gitHubLink: 'https://github.com/Ghrehh/tweetatlas-server'
     },
     'nsts-metronome': {
       name: 'NSTS METRONOME',
       description:
-        'A sleek web metronome capable of playing in non-standard time signatures.',
+        'A sleek web metronome capable of playing in non-standard time signatures. Built using Vue JS',
       backgroundColor: 'var(--purple)',
       gitHubLink: 'https://github.com/Ghrehh/nsts-metronome',
       websiteLink: 'https://nsts-metronome.github.io/'
@@ -33,7 +34,8 @@ export default {
     },
     'vue-world-map': {
       name: 'VUE WORLD MAP',
-      description: 'A Vue.js library for displaying data on a world map',
+      description:
+        'A Vue.js library for displaying heat map style data per country on a world map.',
       backgroundColor: 'var(--blue)',
       gitHubLink: 'https://github.com/Ghrehh/vue-world-map'
     }
