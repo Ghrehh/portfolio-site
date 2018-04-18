a115e773b970f90c142c5c21b6b178979304a631
Gerard Burns <gjburns9@hotmail.com>
2018-04-18 15:42:04 +0100

    change index css file name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'reset-css';
-import './index.module.css';
+import './index.css';
 import Routes from './Routes';
 import serviceWorker from './registerServiceWorker';
 
