25e5ed1913c0578e78b79d3a2ee35d5a5c7650b5
Gerard Burns <gjburns9@hotmail.com>
2018-04-18 16:23:45 +0100

    remove service worker again

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import ReactDOM from 'react-dom';
 import 'reset-css';
 import './index.css';
 import Router from './Router';
-import serviceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
-serviceWorker();
