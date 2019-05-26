2ef021734e96bac2ded15d8dc1675771c71ea4ac
Gerard Burns <gjburns9@hotmail.com>
2019-05-26 04:02:29 +0100

    datA

--- src/data.js
+++ src/data.js
@@ -1,29 +1,13 @@
 // @flow
 export const projects = {
-  'tweetatlas-client': {
-    name: 'TWEETATLAS CLIENT',
-    description:
-      'Client used to display data from the Tweetatlas Server, built using Vue JS.',
-    backgroundColor: 'var(--green)',
-    gitHubLink: 'https://github.com/Ghrehh/tweetatlas-client',
-    tags: ['Vue']
-  },
-  'tweetatlas-server': {
-    name: 'TWEETATLAS SERVER',
-    description:
-      'Service used to estimate the location of users creating tweets containing a search phrase or phrases. Built using Go.',
-    backgroundColor: 'var(--yellow)',
-    gitHubLink: 'https://github.com/Ghrehh/tweetatlas-server',
-    tags: ['Go']
-  },
   'nsts-metronome': {
     name: 'NSTS METRONOME',
     description:
       'A sleek web metronome capable of playing in non-standard time signatures. Built using Vue JS',
-    backgroundColor: 'var(--purple)',
+    backgroundColor: 'var(--green)',
     gitHubLink: 'https://github.com/Ghrehh/nsts-metronome',
     websiteLink: 'https://nsts-metronome.github.io/',
-    tags: ['Vue', 'Music']
+    tags: ['Vue', 'Music', 'CSS animation']
   },
   'ear-training': {
     name: 'EAR TRAINING',
@@ -41,6 +25,23 @@ export const projects = {
     backgroundColor: 'var(--light-blue)',
     gitHubLink: 'https://github.com/Ghrehh/vue-world-map',
     tags: ['Package']
+  },
+  'tweetatlas-client': {
+    name: 'TWEETATLAS CLIENT',
+    description:
+      'Client used to display data from the Tweetatlas Server, built using Vue JS.',
+    backgroundColor: 'var(--yellow)',
+    websiteLink: 'https://tweetatlas.github.io/',
+    gitHubLink: 'https://github.com/Ghrehh/tweetatlas-client',
+    tags: ['Vue', 'WebSockets']
+  },
+  'tweetatlas-server': {
+    name: 'TWEETATLAS SERVER',
+    description:
+      'Service used to estimate the location of users creating tweets containing a search phrase or phrases. Built using Go.',
+    backgroundColor: 'var(--purple)',
+    gitHubLink: 'https://github.com/Ghrehh/tweetatlas-server',
+    tags: ['Go', 'WebSockets']
   },
   'portfolio-website': {
     name: 'PORTFOLIO WEBSITE',
